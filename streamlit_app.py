604a11118353b5d327a010294f17f4fb7b89f31d
Stefan <103984462+Customized-Analytics@users.noreply.github.com>
2022-09-27 22:29:56 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 
-streamlit.title('My Parents New Healthy Dinner')
+streamlit.title('My Parents New Healthy Dinner @Stefan loves Ellen')
 streamlit.header('Breakfast Menu')
 streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
