4a84606ff0f627826e717741d2d3f4d657100075
Stefan <103984462+Customized-Analytics@users.noreply.github.com>
2022-09-27 22:37:57 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,9 @@
-# import diffrent packages we need
-
+# load streamlit
 import streamlit
-import pandas
+
 
 
 # start filling context to the Site
-
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.text('#Stefan loves Ellen')
 streamlit.header('Breakfast Menu')
@@ -13,16 +11,15 @@ streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
 streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
 streamlit.text('🐔 Hard-Boiled Free-range Egg')
 streamlit.text('🥑🍞 Avocado Toast')
-
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
+# load pandas package
+import pandas
 
 # Let's put a pick list here so they can pick the fruit they want to include 
-
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
 
 # Display the table on the page
-
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruit_list)
 
