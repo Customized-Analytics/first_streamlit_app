1592f654371dfa1f1872a83f8120c3d5c2efd9d2
Stefan <103984462+Customized-Analytics@users.noreply.github.com>
2022-10-03 21:39:58 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,12 +49,18 @@ except URLError as e:
     streamlit.error()
 
 # Code to get the Logininformation for the Warehouse out of the Secret-Section
-my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
-my_cur = my_cnx.cursor()
-my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
-my_data_rows = my_cur.fetchall()
 streamlit.header("The Fruit load List contains:")
-streamlit.dataframe(my_data_rows)
+# Snowflake-related functions
+def get_fruit_load_list():
+  with my_cnx.cursor() as my_cur:
+    my_cur.execute("SELECT * FROM FRUIT_LOAD_LIST")
+    return my_cur.fetchall()
+  
+# Add a button to load the fruit
+if streamlit.button('Get Fruit Load List"):
+   my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+   my_data_rows = get_fruit_load_list()
+   streamlit.dataframe(my_data_rows)
 
 # Additional Section to display Text
 fruit_request = streamlit.text_input('What Fruit would you like to add?')
